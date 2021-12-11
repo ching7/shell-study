4300eeafbf45a4ab0fc4d0fa4f15723e24de7933
ching <775608598@qq.com>
2021-12-11 16:31:35 +0800

    sh update

--- 07-expr.sh
+++ 07-expr.sh
@@ -33,6 +33,7 @@ then
 fi
 
 ############################### 关系运算
+# 只支持数字 不支持字符串
 a=10
 b=20
 
@@ -68,4 +69,95 @@ then
 	echo  "$a -ge $b:a大于等于b"
 else
 	echo "$a -ge $b:a小于b"
+fi
+if [ $a -le $b ]
+then
+	echo  "$a -ge $b:a小于等于b"
+else
+	echo "$a -ge $b:a大于b"
+fi
+
+# 布尔运算 ##########
+a=10
+b=20
+if [ $a != $b ]
+then
+	echo "$a != $b :  a 不等于 b"
+else
+	echo "$a == $b :  a 不等于 b"
+fi
+
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+	echo "$a < 100 and $b > 15 :  return true"
+else
+	echo "$a < 100 and $b > 15 :  return false"
+fi
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+	echo "$a < 100 and $b > 15 :  return true"
+else
+	echo "$a < 100 and $b > 15 :  return false"
+fi
+if [ $a -lt 100 -o $b -gt 15 ]
+then
+	echo "$a < 100 or $b > 100 :  return true"
+else
+	echo "$a < 100 or $b > 100 :  return false"
+fi
+if [ $a -lt 5 -a $b -gt 100 ]
+then
+	echo "$a < 5 or $b > 100 :  return true"
+else
+	echo "$a < 5 or $b > 100 :  return false"
+fi
+# 逻辑运算符,注意需要使用2个[[]]
+a=10
+b=20
+if [[ $a -lt 100 && $b -gt 100 ]]
+then
+	echo "$a<100 && $b>100 : true"
+else
+	echo "$a<100 && $b>100 : false"
+fi
+
+if [[ $a -lt 100 || $b -gt 100 ]]
+then
+	echo "$a<100 || $b>100 : true"
+else
+	echo "$a<100 || $b>100 : false"
+fi
+## 字符串运算
+a="abc"
+b="efg"
+if [ $a = $b ]
+then
+	echo "$a=$b : a=b"
+else
+	echo "$a=$b : a!=b"
+fi
+
+if [ $a != $b ]
+then
+	echo "$a!=$b : a!=b"
+else
+	echo "$a!=$b : a=b"
+fi
+if [ -z $a ]
+then
+	echo "-z $a : a length = 0"
+else
+	echo "-z $a : a length != 0"
+fi
+if [ -n "$a" ]
+then
+	echo "-n $a : a length != 0"
+else
+	echo "-n $a : a length =0"
+fi
+if [ $a ]
+then
+	echo "$a  : a !=null"
+else
+	echo "$a : a=null"
 fi
